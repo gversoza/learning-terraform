c1440fdec116cbbe532e549d524e44cd1ce7f8f0
Gabriel Versoza <6806752+gversoza@users.noreply.github.com>
2025-06-19 12:53:35 +0800

    define qa environment

---
+++ qa/outputs.tf
@@ -0,0 +1,12 @@
+
+output "instance_ami" {
+  value = aws_instance.blog.ami
+}
+
+output "instance_arn" {
+  value = aws_instance.blog.arn
+}
+
+output "environment_url" {
+    value = module.qa.environment_url
+}
