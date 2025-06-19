29b24d688d0a0b0738bbce7e2f9c842a17fb2f44
Gabriel Versoza <6806752+gversoza@users.noreply.github.com>
2025-06-19 10:43:37 +0800

    update target group

--- main.tf
+++ main.tf
@@ -32,17 +32,22 @@ module "blog_vpc" {
 
 module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
-  version = "6.5.2"
 
   name = "blog"
 
   min_size            = 1
   max_size            = 2
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns   = module.blog_alb.target_group_arns
   security_groups     = [module.blog_sg.security_group_id]
   instance_type       = var.instance_type
   image_id            = data.aws_ami.app_ami.id
+
+  traffic_source_attachments = {
+    alb_target_group = {
+      type = "elb"
+      traffic_source_identifier = module.blog_alb.target_groups.ex-instance.arn
+    }
+  }
 }
 
 module "blog_alb" {
